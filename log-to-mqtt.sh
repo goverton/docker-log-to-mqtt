9992c1015b431907bdd26c022a0fb832e8869094
goverton <graham.overton@gmail.com>
2018-10-05 11:47:30 +0100

    Update log-to-mqtt.sh

--- log-to-mqtt.sh
+++ log-to-mqtt.sh
@@ -1,44 +1,22 @@
 #!/bin/sh
 
+LOG=/logs/log-to-mqtt.log
+touch $LOG
+
 # Unifi Video Vars
 UNIFI_MOTION_LOG=/unifilogs/motion.log
 
 # MQTT Vars
 MQTT_SERVER="192.168.1.100"
+MQTT_PORT="1883"
 MQTT_TOPIC_BASE="camera/motion"
-echo "MQTT $MQTT_SERFVER topic $MQTT_TOPIC_BASE"
 
-# MQTT User/Pass Vars, only use if needed
-#MQTT_USER="username"
-#MQTT_PASS="password"
-
-# Camera Defs
-CAM1_NAME="Driveway - Garage"
-CAM1_ID="F09FC2C0ED58"
-echo "Scanning $UNIFI_MOTION_LOG for $CAM1_NAME $CAM1_ID"
-
-# --------------------------------------------------------------------------------
-# Script starts here
-
-if [[ -n "$MQTT_USER" && -n "$MQTT_PASS" ]]; then
-  $MQTT_USER_PASS="-u $MQTT_USER -P $MQTT_PASS"
-else
-  $MQTT_USER_PASS=""
-fi
+echo "`date -I'seconds'` [INFO] - MQTT $MQTT_SERVER:$MQTT_PORT topic $MQTT_TOPIC_BASE"
 
 while inotifywait -e modify $UNIFI_MOTION_LOG; do
-  LAST_MESSAGE=`tail -n1 $UNIFI_MOTION_LOG`
-  LAST_CAM=`echo $LAST_MESSAGE | awk -F '[][]' '{print $2}'`
-  LAST_EVENT=`echo $LAST_MESSAGE | cut -d ':' -f 5 | cut -d ' ' -f 1`
+  LAST_LOG_MESSAGE=`tail -n1 $UNIFI_MOTION_LOG`
+  CAMERA=`echo $LAST_LOG_MESSAGE | awk -F '[()]' '{print $2}'`
+  ACTION=`echo $LAST_LOG_MESSAGE | sed -n 's/.*type:\([a-z][a-z]*\) .*/\1/p'`
 
-  if echo $LAST_CAM | grep $CAM1_ID; then
-    # Camera 1 triggered
-	  if [[ $LAST_EVENT == "start" ]]; then
-	    echo "Motion started on $CAM1_NAME"
-	    mosquitto_pub -h $MQTT_SERVER $MQTT_USER_PASS -r -t $MQTT_TOPIC_BASE/$CAM1_NAME -m "ON" &
-	  else
-	    echo "Motion stopped on $CAM1_NAME"
-	    mosquitto_pub -h $MQTT_SERVER $MQTT_USER_PASS -r -t $MQTT_TOPIC_BASE/$CAM1_NAME -m "OFF" &
-	  fi
-  fi
+  mosquitto_pub -h "$MQTT_SERVER" -p "$MQTT_PORT" -r -t "$MQTT_TOPIC_BASE/$CAMERA" -m "$ACTION" &
 done
